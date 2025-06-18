5e914e1593a82610471fa99ae88051ef9fc3d5a8
Peder Frang <peder.frang@gmail.com>
2025-06-18 14:49:55 +0200

    Uncomment plugins for nvim

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'numToStr/Comment.nvim',
+  opts = {
+    -- add any options here
+  },
+}
